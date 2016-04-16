0f66b35469e37c9f9e9473c47d1c7eb08d5a3b40
RGrun <richard@r3software.org>
2016-04-15 19:03:44 -0700

    Initial commit

---
+++ src/clj_partybus/core.clj
@@ -0,0 +1,57 @@
+(ns clj-partybus.core
+  (:require [clojure.core.async :refer [chan <!! >!! go-loop]]))
+
+
+(def queues (atom {}))
+
+
+(defn register!
+  "Registers a thread with the message bus."
+  [key]
+  (if (keyword? key)
+    (if-not (contains? @queues key)
+      (swap! queues assoc key clojure.lang.PersistentQueue/EMPTY)
+      (throw
+        (Exception. "Queue already exists for this key.")))
+    (throw
+      (Exception. "Key must be a keyword."))))
+
+(defn clear-queue!
+  "Clears the queue belonging to a specific key."
+  [key]
+  (if (contains? @queues key)
+    (swap! queues assoc key clojure.lang.PersistentQueue/EMPTY)
+    (throw
+      (Exception. "This key isn't registsterd."))))
+
+(defn full-reset!
+  "Clears all queues and keys."
+  []
+  (reset! queues {}))
+
+(defn message-ready?
+  "Returns true if there is a message ready for the given key."
+  [key]
+  (if (contains? @queues key)
+    (not (nil? (peek (@queues key))))
+    (throw
+      (Exception. "This key isn't registered."))))
+
+(defn send-message!
+  "Adds a new message to the queue belonging to the given key."
+  [key msg]
+  (if (contains? @queues key)
+    (swap! queues #(update % key conj msg))
+    (throw
+      (Exception. "There is no queue registered for this key."))))
+
+
+(defn get-message!
+  "Grabs the next avaliable message in key's queue."
+  [key]
+  (if (message-ready? key)
+    (let [ret (peek (@queues key))]
+      (swap! queues #(update % key pop))
+      ret)
+    nil                                                     ;; throw exception instead?
+    ))
